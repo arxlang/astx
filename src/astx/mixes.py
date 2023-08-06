2b3a4dddffd821062e880e36ae0cdfcaf7b52848
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-08-06 10:35:09 -0400

    Update -NamedExpr

--- src/astx/mixes.py
+++ src/astx/mixes.py
@@ -1,9 +1,10 @@
 from typing import TypeAlias, Union
 
+from astx.base import DataType
 from astx.datatypes import Variable
 from astx.callables import Function
 
 
 __all__ = ["NamedExpr"]
 
-NamedExpr: TypeAlias = Union[Function, Variable]
+NamedExpr: TypeAlias = Union[DataType, Function, Variable]
