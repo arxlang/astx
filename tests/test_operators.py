b09f2882775ca1e02ee028ad803026ad282264b7
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-10-25 17:58:00 -0400

    test: Add more tests for binary_op (#17)

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,13 +1,39 @@
 """Module for testing operators."""
+import pytest
+
 from astx.datatypes import Int32Literal
 from astx.operators import BinaryOp, UnaryOp
 
+lit_1 = Int32Literal(1)
+lit_2 = Int32Literal(2)
+lit_3 = Int32Literal(3)
 
-def test_binary_op() -> None:
+
+@pytest.mark.parametrize(
+    "explicit,implicit",
+    [
+        (BinaryOp(op_code="+", lhs=lit_1, rhs=lit_2), lit_1 + lit_2),
+        (
+            BinaryOp(
+                op_code="+",
+                lhs=lit_1,
+                rhs=BinaryOp(op_code="+", lhs=lit_2, rhs=lit_3),
+            ),
+            lit_1 + (lit_2 + lit_3),
+        ),
+        (
+            BinaryOp(
+                op_code="+",
+                lhs=BinaryOp(op_code="+", lhs=lit_1, rhs=lit_2),
+                rhs=lit_3,
+            ),
+            lit_1 + lit_2 + lit_3,
+        ),
+    ],
+)
+def test_binary_op(explicit: BinaryOp, implicit: BinaryOp) -> None:
     """Test binary operator."""
-    lit_a = Int32Literal(1)
-    lit_b = Int32Literal(2)
-    BinaryOp(op_code="+", lhs=lit_a, rhs=lit_b)
+    assert implicit.get_struct() == explicit.get_struct()
 
 
 def test_unary_op() -> None:
