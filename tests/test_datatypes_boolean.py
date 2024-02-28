5b84b04fe047e556a3446e380032a88d650a0013
Ana Paula Krelling <74330736+apkrelling@users.noreply.github.com>
2024-02-27 22:06:03 -0500

    feat: create LiteralBoolean class (#51)
    ---------
    
    Co-authored-by: Ana Krelling <ana@axiomdatascience.com>

---
+++ tests/test_datatypes_boolean.py
@@ -0,0 +1,17 @@
+"""Tests for Boolean data type."""
+from astx.datatypes import Boolean, LiteralBoolean
+from astx.variables import VariableDeclaration
+
+
+def test_variable_boolean() -> None:
+    """Test variable boolean."""
+    decl_a = VariableDeclaration(
+        name="a", type_=Boolean, value=LiteralBoolean(value=True)
+    )
+    assert decl_a.type_ == Boolean.__mro__[0]  # astx.datatypes.Boolean
+
+
+def test_literal_boolean() -> None:
+    """Test literal boolean."""
+    lit_a = LiteralBoolean(value=True)
+    assert type(lit_a) == LiteralBoolean
