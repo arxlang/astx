c2ed382e0a00ad140cb19455aade018492d9d4ea
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-04-21 22:35:32 -0400

    update config and deps

--- scripts/gen_ref_nav.py
+++ scripts/gen_ref_nav.py
@@ -12,7 +12,9 @@ from pathlib import Path
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
-mod_symbol = '<code class="doc-symbol doc-symbol-nav doc-symbol-module"></code>'
+mod_symbol = (
+    '<code class="doc-symbol doc-symbol-nav doc-symbol-module"></code>'
+)
 
 root = Path(__file__).parent.parent
 src = root / "src"
@@ -20,7 +22,7 @@ src = root / "src"
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src / "astx").with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)
+    full_doc_path = Path("api", doc_path)
 
     parts = tuple(module_path.parts)
 
@@ -38,7 +40,9 @@ for path in sorted(src.rglob("*.py")):
         ident = ".".join(parts)
         fd.write(f"::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path.relative_to(root))
+    mkdocs_gen_files.set_edit_path(
+        full_doc_path, ".." / path.relative_to(root)
+    )
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("api/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
