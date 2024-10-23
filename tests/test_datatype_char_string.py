be6bee2109b36457c319161ffc4a42721395d7f7
vikasgrewal16 <vv172002kumar@gmail.com>
2024-10-23 18:54:12 +0530

    Pushing test for utf8 base classes

--- tests/test_datatype_char_string.py
+++ tests/test_datatype_char_string.py
@@ -14,10 +14,12 @@ VAR_A = Variable("a")
 
 UTF8_CHAR_LITERAL_CLASSES = [
     astx.LiteralUTF8Char,
+    astx.UTF8Char,
 ]
 
 UTF8_STRING_LITERAL_CLASSES = [
     astx.LiteralUTF8String,
+    astx.UTF8String,
 ]
 
 
