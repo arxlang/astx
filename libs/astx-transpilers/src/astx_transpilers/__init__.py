9f808bed30c1d436e11bd9c013835e71b2c563b8
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2025-03-27 11:53:28 -0400

    fix: Fix the release workflow (2) (#228)

--- libs/astx-transpilers/src/astx_transpilers/__init__.py
+++ libs/astx-transpilers/src/astx_transpilers/__init__.py
@@ -1,1 +1,18 @@
 """ASTx Transpilers."""
+
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    """Return the program version."""
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.19.0"  # semantic-release
+
+
+version: str = get_version()
+
+__author__ = "Ivan Ogasawara"
+__email__ = "ivan.ogasawara@gmail.com"
+__version__: str = version
