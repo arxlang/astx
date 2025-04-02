7650e4a6ebf3eb5bb840f3c9bdef6976276888d4
Alexis-Dubois <163043930+Alexis-Dubois@users.noreply.github.com>
2025-04-02 17:28:57 -0400

    docs: Update usage demo in README.md and added a test for it (#235)

---
+++ libs/astx/tests/test_readme_usage.py
@@ -0,0 +1,46 @@
+"""Test to see if the usage demo in README.md still works."""
+
+import textwrap
+
+
+def usage_demo() -> str:
+    """Execute an exact copy of the code shown in README.md."""
+    import astx
+
+    # Define a simple function `add(x, y): return x + y`
+    args = astx.Arguments(
+        astx.Argument(name="x", type_=astx.Int32()),
+        astx.Argument(name="y", type_=astx.Int32()),
+    )
+    fn_body = astx.Block()
+    fn_body.append(
+        astx.FunctionReturn(
+            value=astx.BinaryOp(
+                op_code="+", lhs=astx.Variable("x"), rhs=astx.Variable("y")
+            )
+        )
+    )
+    add_function = astx.FunctionDef(
+        prototype=astx.FunctionPrototype(
+            name="add", args=args, return_type=astx.Int32()
+        ),
+        body=fn_body,
+    )
+
+    from astx_transpilers.python_string import ASTxPythonTranspiler
+
+    # Transpile the AST to Python
+    transpiler = ASTxPythonTranspiler()
+    python_code = transpiler.visit(add_function)
+
+    return str(python_code)
+
+
+def test_readme_usage() -> None:
+    """Test usage demo as shown in README.md."""
+    expected = """\
+                def add(x: int, y: int) -> int:
+                    return (x + y)
+                """
+
+    assert usage_demo() == textwrap.dedent(expected).strip()
