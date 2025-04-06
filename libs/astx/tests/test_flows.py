c163c4c5a04341658c3a4b79b22bcb832ddea0e7
Mehendi Hasan <117719513+Zehen-249@users.noreply.github.com>
2025-04-06 06:20:04 +0530

    feat: Add Generator Expr support (#232)

--- libs/astx/tests/test_flows.py
+++ libs/astx/tests/test_flows.py
@@ -386,3 +386,54 @@ def test_do_while_stmt() -> None:
     assert do_while_stmt.get_struct()
     assert do_while_stmt.get_struct(simplified=True)
     visualize(do_while_stmt.get_struct())
+
+
+def test_generator_expr_1() -> None:
+    """Test `GeneratorExpr` class with conditions of Iterable type."""
+    gen_expr = astx.GeneratorExpr(
+        element=astx.BinaryOp(
+            op_code="+", lhs=astx.Variable("x"), rhs=astx.Variable("x")
+        ),
+        target=astx.Variable("x"),
+        iterable=astx.Identifier("range(10)"),
+        conditions=[
+            astx.BinaryOp(
+                op_code="<", lhs=astx.Variable("x"), rhs=astx.LiteralInt32(8)
+            ),
+            astx.BinaryOp(
+                op_code="%", lhs=astx.Variable("x"), rhs=astx.LiteralInt32(2)
+            ),
+        ],
+    )
+    assert str(gen_expr)
+    assert gen_expr.get_struct()
+    assert gen_expr.get_struct(simplified=True)
+    visualize(gen_expr.get_struct())
+
+
+def test_generator_expr_2() -> None:
+    """Test `GeneratorExpr` class with conditions of ASTNodes type."""
+    conditions = astx.ASTNodes[astx.Expr]()
+    conditions.append(
+        astx.BinaryOp(
+            op_code="<", lhs=astx.Variable("x"), rhs=astx.LiteralInt32(8)
+        )
+    )
+    conditions.append(
+        astx.BinaryOp(
+            op_code="%", lhs=astx.Variable("x"), rhs=astx.LiteralInt32(2)
+        )
+    )
+    gen_expr = astx.GeneratorExpr(
+        element=astx.BinaryOp(
+            op_code="+", lhs=astx.Variable("x"), rhs=astx.Variable("x")
+        ),
+        target=astx.Variable("x"),
+        iterable=astx.Identifier("range(10)"),
+        conditions=conditions,
+    )
+    print(gen_expr)
+    assert str(gen_expr)
+    assert gen_expr.get_struct()
+    assert gen_expr.get_struct(simplified=True)
+    visualize(gen_expr.get_struct())
