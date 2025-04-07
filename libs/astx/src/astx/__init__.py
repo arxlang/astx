2074103b9d0a1e300342eb417986eefff34cae4c
Zehen-249 <1hasanmehendi123@gmail.com>
2025-04-08 02:15:21 +0530

    fix: fix GeneratorExp class to inherit from comprehension class

--- libs/astx/src/astx/__init__.py
+++ libs/astx/src/astx/__init__.py
@@ -56,6 +56,7 @@ from astx.classes import (
 from astx.comprehensions import (
     Comprehension,
     ComprehensionClause,
+    GeneratorExpr,
     ListComprehension,
 )
 from astx.exceptions import (
@@ -74,7 +75,6 @@ from astx.flows import (
     ForCountLoopStmt,
     ForRangeLoopExpr,
     ForRangeLoopStmt,
-    GeneratorExpr,
     GotoStmt,
     IfExpr,
     IfStmt,
